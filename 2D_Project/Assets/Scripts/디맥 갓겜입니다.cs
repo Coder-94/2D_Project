0265014e1bf31739386a7a82c5aa199de768c3ae
skyfish811 <zeoryubin@gmail.com>
2023-08-04 11:15:56 +0900

    먼 개소리야

---
+++ 2D_Project/Assets/Scripts/디맥 갓겜입니다.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 디맥갓겜입니다 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
